470801f864768e41ef4f571c67cdf7c008af0f81
lukelliotfrost <luke@radio.co>
2019-09-21 15:32:16 +0100

    Created file model and relationship between File and User classes

--- app/File.php
+++ app/File.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class File extends Model
 {
-    //
+    protected $fillable = [
+      'title',
+      'overview'
+    ];
+
+    public function user()
+    {
+      return $this->belongsTo(User::class);
+    }
 }
